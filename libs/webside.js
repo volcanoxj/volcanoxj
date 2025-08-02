7bd3011dfe80cb43c9c28628ea941f70a5fefc9d
xjunster <xjunster@gmail.com>
2025-08-02 01:03:36 +0000

    修复文章中相对路径

--- libs/webside.js
+++ libs/webside.js
@@ -9,7 +9,8 @@ async function build({title, items}) {
         category.items.forEach(function(book){
             book.items.forEach(async function(article){
                 fs.mkdirSync(path.dirname(`docs/${article.uri}.html`), {recursive: true});
-                let data = await ejs.renderFile(`${__dirname}/views/article.ejs`, {keys:[category.title, book.title, article.title], article:article});
+                let rootdir = path.dirname(article.uri).replace(/\w+/g, '..');
+                let data = await ejs.renderFile(`${__dirname}/views/article.ejs`, {keys:[category.title, book.title, article.title], article:article, rootdir:rootdir});
                 fs.writeFileSync(`docs/${article.uri}.html`, data);
             });
         });
